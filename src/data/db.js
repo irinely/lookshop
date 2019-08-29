1542e843a339f45a392ed3a65d5ba6b7ead9e71e
Irene Artemova <artemova.irene@gmail.com>
2019-08-29 15:05:28 +0400

    Адаптирование страницы продукта (компонент ProductPage)

--- src/data/db.js
+++ src/data/db.js
@@ -5,7 +5,7 @@ export default {
 			date: "08.08.2019",
 			title: "Gooding shorts",
 			description: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.",
-			images: ["/images/products/2.jpg"],
+			images: ["/images/products/2.jpg", "/images/products/1.jpg"],
 			designers: [
 				1
 			],
@@ -28,7 +28,7 @@ export default {
 			date: "08.06.2019",
 			title: "Feel Sweaters-Blouse",
 			description: "This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.",
-			images: ["/images/products/1.jpg"],
+			images: ["/images/products/1.jpg", "/images/products/2.jpg"],
 			designers: [
 				1
 			],
